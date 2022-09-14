3d83ed42fe2780e78c7367d4ecc63e19e4434118
jolo-dev <john.nguyen@kreuzwerker.de>
2022-09-14 23:40:18 +0200

    Add LambdaStack

---
+++ src/LambdaStack.ts
@@ -0,0 +1,10 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class LambdaStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    // define resources here...
+  }
+}
